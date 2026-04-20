a85c8c88e73e329e56b64b64088b12a8b79712a2
Anusha Akhtar <anushaakhtar@Anushas-MacBook-Pro.local>
2026-04-20 00:27:20 -0400

    Uploading critical thinking assignment 1

---
+++ personinfo.java
@@ -0,0 +1,19 @@
+package module1;
+
+public class personinfo {
+
+	public static void main(String[] args) {
+
+		String firstName = "Anusha";
+		String lastName = "Akhtar";
+		String streetAddress = "543 Sunset Blvd";
+		String city = "Chicago";
+		String zipCode = "60601";
+
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAddress);
+		System.out.println(city);
+		System.out.println(zipCode);
+	}
+}
